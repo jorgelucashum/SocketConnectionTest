fa3a9e77da7b44bcce6a75ef711d60d1bb39819b
Jorge Lucas <jorgelucashum@gmail.com>
2021-04-06 16:44:56 -0300

    Projeto finalizado

--- TCP2/Form1.Designer.cs
+++ TCP2/Form1.Designer.cs
@@ -33,26 +33,24 @@ namespace TCP2
             this.btnStart = new System.Windows.Forms.Button();
             this.txtServer = new System.Windows.Forms.Label();
             this.txtInfo = new System.Windows.Forms.TextBox();
-            this.txtMessage = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnSend = new System.Windows.Forms.Button();
             this.lstClientIP = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(67, 26);
+            this.txtIP.Location = new System.Drawing.Point(58, 13);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(326, 20);
+            this.txtIP.Size = new System.Drawing.Size(355, 20);
             this.txtIP.TabIndex = 0;
             this.txtIP.Text = "127.0.0.1:9000";
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(318, 254);
+            this.btnStart.Location = new System.Drawing.Point(457, 261);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(66, 24);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Iniciar";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -61,7 +59,7 @@ namespace TCP2
             // txtServer
             // 
             this.txtServer.AutoSize = true;
-            this.txtServer.Location = new System.Drawing.Point(12, 29);
+            this.txtServer.Location = new System.Drawing.Point(3, 16);
             this.txtServer.Name = "txtServer";
             this.txtServer.Size = new System.Drawing.Size(49, 13);
             this.txtServer.TabIndex = 2;
@@ -69,74 +67,56 @@ namespace TCP2
             // 
             // txtInfo
             // 
-            this.txtInfo.Location = new System.Drawing.Point(67, 52);
+            this.txtInfo.Location = new System.Drawing.Point(29, 39);
             this.txtInfo.Multiline = true;
             this.txtInfo.Name = "txtInfo";
             this.txtInfo.ReadOnly = true;
             this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtInfo.Size = new System.Drawing.Size(326, 160);
+            this.txtInfo.Size = new System.Drawing.Size(384, 199);
             this.txtInfo.TabIndex = 0;
-            // 
-            // txtMessage
-            // 
-            this.txtMessage.Location = new System.Drawing.Point(67, 218);
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(326, 20);
-            this.txtMessage.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-1, 221);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Mensagem:";
-            // 
-            // btnSend
-            // 
-            this.btnSend.Location = new System.Drawing.Point(237, 254);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 1;
-            this.btnSend.Text = "Enviar";
-            this.btnSend.UseVisualStyleBackColor = true;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // lstClientIP
             // 
             this.lstClientIP.FormattingEnabled = true;
-            this.lstClientIP.Location = new System.Drawing.Point(399, 52);
+            this.lstClientIP.Location = new System.Drawing.Point(419, 39);
             this.lstClientIP.Name = "lstClientIP";
-            this.lstClientIP.Size = new System.Drawing.Size(185, 186);
+            this.lstClientIP.Size = new System.Drawing.Size(104, 199);
             this.lstClientIP.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(399, 29);
+            this.label2.Location = new System.Drawing.Point(419, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "IPs Conectados:";
             // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(351, 261);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 24);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Salvar arquivo .txt";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 286);
+            this.ClientSize = new System.Drawing.Size(535, 297);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.lstClientIP);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtServer);
-            this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtInfo);
-            this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.txtIP);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "TCP Server";
+            this.Text = "Slave (TCP Server)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -149,11 +129,9 @@ namespace TCP2
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label txtServer;
         private System.Windows.Forms.TextBox txtInfo;
-        private System.Windows.Forms.TextBox txtMessage;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.ListBox lstClientIP;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
